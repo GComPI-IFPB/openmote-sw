a988fd1d5444016692a9b71180a47a2f35f2ccea
Pere Tuset <peretuset@gmail.com>
2019-07-24 12:37:49 +0200

    Add cloud-server project.

---
+++ cloud-server/config.py
@@ -0,0 +1,20 @@
+mqtt_config = {
+    "mqtt_id": "",
+    "mqtt_address": "",
+    "mqtt_port": ,
+    "mqtt_subscribe_topics": [""],
+    "mqtt_publish_topic": ""
+}
+
+packet_config = {
+    "timeout_ms": 0,
+    "all_keys": {""},
+    "keep_keys": [""]
+}
+
+calibration_config = {
+    "use_calibration": False
+}
+
+calibration_values = {
+}
