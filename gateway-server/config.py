2244a06e28082569a9738ec08f0d3826af4f2e6d
Pere Tuset <peretuset@gmail.com>
2019-07-24 12:40:04 +0200

    Add gateway-server project.

---
+++ gateway-server/config.py
@@ -0,0 +1,21 @@
+serial_config = {
+    "serial_devices": 1,
+    "serial_baudrate": 1267200,
+    "serial_timeout": 0.1
+}
+
+mqtt_config = {
+    "mqtt_address": "",
+    "mqtt_port": ,
+}
+
+network_config = {
+    "pan_id": ""
+}
+
+message_config = {
+    "message_id"        : "",
+    "message_topic"     : "",
+    "message_fields"    : [""],
+    "message_structure" : ""
+}
